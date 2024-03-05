3c012111ee9079ea6f9b647c1123c2ca1aa1774b
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-03-05 18:05:01 +0530

    Update SwapCase.py

--- SwapCase.py
+++ SwapCase.py
@@ -1,3 +1,5 @@
 text = input("Enter the string : ")
+
+#Swaps the cases that is, if the input string is in lowercase that will swaps to uppercase and vice-versa.
 swap_case = text.swapcase()
 print(swap_case)
