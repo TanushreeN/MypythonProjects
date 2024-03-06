1cb1803b297e346ba7b158b08e0796ae4c7b42fc
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-03-06 19:42:00 +0530

    Simple Interest program

---
+++ Simple_interest.py
@@ -0,0 +1,12 @@
+def Simple_interest(p, t, r):
+
+# To calculate Simple interest
+    si = (p * t * r) / 100
+    print("The Simple Interest is : ",si)
+    return si
+
+Simple_interest(1000, 1, 10)
+
+
+
+
