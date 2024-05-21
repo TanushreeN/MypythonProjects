26823dc79ae176d12871ad1f79f52db0f285e78e
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-05-22 05:14:51 +0530

    Addition of number with its previous and next no.

---
+++ addition_of_number_with_previous_and_next_number.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a Number : "))
+b = int(number) - 1
+c = int(number) + 1
+d = number + b + c
+print(b  , "+" , number , "+" , c ,"is given below")
+print("The sum of the number along with its previous and next number is : ", d )
