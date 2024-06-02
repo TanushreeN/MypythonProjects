5edf4dabcf74857c9860b0c3bc6892db36c58c8b
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-06-02 16:35:55 +0530

    Create mult_def.py

---
+++ mult_def.py
@@ -0,0 +1,14 @@
+# Multiply using function
+__________________________
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+    
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+print(f"The multiplication of {num1} and {num2} is:", multiply(num1, num2))
+
+__________________________
+# OP:   Enter num1: 3
+#       Enter num2: 4
+#        The multiplication of 3 and 4 is: 12
