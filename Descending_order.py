175997c39fd46c0d8938816d3e9eedcc5e1acb98
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-12-27 20:50:34 +0530

    Descending_order.py

--- Descending_order.py
+++ Descending_order.py
@@ -2,6 +2,6 @@
 
 
 numbers = [11, 67, 10, 27, 31, 6, 1, 4]
-ascending_order =  sorted(numbers, reverse = True)
+descending_order =  sorted(numbers, reverse = True)
 
-print("Sorting in Descending order = ",ascending_order)
+print("Sorting in Descending order = ",descending_order)
